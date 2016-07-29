0383902d6b038e3f370b0205f6fb28d771204bdc
Satyajeet Parida <shatyajeet@gmail.com>
2016-07-29 15:51:49 +0530

    Reformatted to use tabs

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	next();
 });
 
 app.options('*', function (req, res) {
