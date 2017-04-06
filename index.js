7f62c7da0b22b18a24d46db6de2ec7dbc57577ef
sangkeon <sangkenlee@gmail.com>
2017-04-06 15:19:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	if(req.header()) {
+	if(req.header('Access-Control-Allow-Headers')) {
 	} else {
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	}
